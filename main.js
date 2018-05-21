f7033c2190bbeea6a2dca0907d57d7346a92665a
oded levy <odedline@hotmail.com>
2018-05-21 13:03:43 +0300

    12345

--- main.js
+++ main.js
@@ -34,6 +34,10 @@ function initMap() {
 
 document.querySelector('.userStatus').innerHTML='you are walking!';
 
+setTimeout(() => {
+  document.querySelector('.userStatus').innerHTML='you are driving!';
+}, 5000)
+
 
 // var service = new google.maps.DistanceMatrixService();
 // setInterval(() => {
